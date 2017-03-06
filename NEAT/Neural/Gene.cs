5cc2460fa446e3e516de45f2cc98df034fc6f802
netdex <netdex593@gmail.com>
2017-03-06 18:12:39 -0500

    Implemented basic Gene structure

---
+++ NEAT/Neural/Gene.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEAT.Neural
+{
+    struct Gene : IComparable<Gene>, IEquatable<Gene>
+    {
+        private static int GlobalInnovation = 0;
+
+        public int Innovation { get; private set; }
+        public int Source { get; set; }
+        public int Destination { get; set; }
+        public double Weight { get; set; }
+
+        public Gene(int s, int d, double w)
+        {
+            Source = s;
+            Destination = d;
+            Weight = d;
+            Innovation = GlobalInnovation++;
+        }
+
+        public int CompareTo(Gene other)
+        {
+            return Innovation - other.Innovation;
+        }
+
+        public bool Equals(Gene other)
+        {
+            return Innovation == other.Innovation;
+        }
+
+        public static bool operator ==(Gene a, Gene b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Gene a, Gene b)
+        {
+            return !a.Equals(b);
+        }
+    }
+}
---
+++ NEAT/Neural/Genome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEAT.Neural
+{
+    class Genome
+    {
+        public Random RandomSource = new Random();
+        public SortedSet<Gene> GeneticInfo { get; set; } = new SortedSet<Gene>();
+        public int NeuronCount { get; set; } = 0;
+
+        private const double EXCESS_WEIGHT = 1.0;
+        private const double DISJOINT_WEIGHT = 1.0;
+        private const double WEIGHT_DIFF_WEIGHT = 0.4;
+
+        public Genome()
+        {
+            
+        }
+
+        public static double GetCompatibility(Genome a, Genome b)
+        {
+            int disjoint = 0;
+            int excess = 0;
+            SortedSet<Gene> disj =new SortedSet<Gene>(a.GeneticInfo);
+            disj.SymmetricExceptWith(b.GeneticInfo);
+            int maxInnovation = Math.Min(a.GeneticInfo.Last().Innovation, b.GeneticInfo.Last().Innovation);
+            
+            foreach (Gene g in disj)
+            {
+                if (g.Innovation > maxInnovation)
+                    excess++;
+                else
+                    disjoint++;
+            }
+            SortedSet<Gene> common = new SortedSet<Gene>(a.GeneticInfo);
+            common.IntersectWith(b.GeneticInfo);
+            return 0;
+        }
+    }
+}
